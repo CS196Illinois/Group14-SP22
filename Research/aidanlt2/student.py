6f42ba9c92ef64154e163f445d1897ef2957b30e
Aidan Toscano <76238720+AidanToscano@users.noreply.github.com>
2022-03-23 20:04:47 -0500

    update

---
+++ Research/aidanlt2/student.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+    def getName(self):
+        return self.name
+    def getYear(self):
+        return self.year
+    def setYear(self, new_year):
+        self.year = new_year
+
+student_1 = Student("Aidan", "freshman")
+print(student_1.getName())
