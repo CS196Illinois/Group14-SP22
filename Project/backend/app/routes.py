e36d1aafa0bd03ceb58dfcd3a0873f038092cab2
Hectorshi <90988360+Hectorshi@users.noreply.github.com>
2022-04-10 17:06:34 -0500

    Backend change

--- Project/backend/app/routes.py
+++ Project/backend/app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-import stock.py
+from stock import *
 
 @app.route('/')
 @app.route('/index')
@@ -12,3 +12,19 @@ def getplayerstat(name):
     playerstat = {'s': 1}
     for key in playerstat.keys():
         print(key, playerstat[key])
+
+@app.route('/test')
+def test():
+    d = datetime(2018, 6, 8)
+    test_stock = {
+    "pts" : 1,
+    "reb" : 1,
+    "ast" : 1,
+    "stl" : 1,
+    "blk" : 1,
+    "tov" : 1 
+    }
+    test_player = player_stock(test_stock, 'jamesle01', d)
+    test = test_player.magic
+    print(test)
+    return render_template('test.html', title = "test", test_player = test_player)
