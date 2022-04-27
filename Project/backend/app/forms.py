708aacbbf1e47f047620668b68fdcf44814345e2
Hectorshi <90988360+Hectorshi@users.noreply.github.com>
2022-04-27 17:21:15 -0500

    add date time

---
+++ Project/backend/app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    playername = StringField('Playername', validators=[DataRequired()])
+    year = StringField('year', validators=[DataRequired()])
+    month = StringField('month', validators=[DataRequired()])
+    date = StringField('date', validators=[DataRequired()])
+    submit = SubmitField('Submit')
